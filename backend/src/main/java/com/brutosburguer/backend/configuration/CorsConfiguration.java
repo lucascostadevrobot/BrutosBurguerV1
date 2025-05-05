abdefb79a8a4ddebbe83bb4e0825c8c3f12ad2c1
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2025-05-05 11:07:30 -0300

    Altera o dominio no CorsConfiguration

--- backend/src/main/java/com/brutosburguer/backend/configuration/CorsConfiguration.java
+++ backend/src/main/java/com/brutosburguer/backend/configuration/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
                  registry.addMapping("/**")
-                .allowedOrigins("https://brutos-burguer-v1-frontend.vercel.app")
+                .allowedOrigins("https://brutos-burguer-v1.vercel.app/") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
