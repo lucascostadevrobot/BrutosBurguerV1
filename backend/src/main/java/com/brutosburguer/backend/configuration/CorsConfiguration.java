7da3754b4fc382ba4d2cd2d5051b3bc60b564028
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2025-05-05 09:33:47 -0300

    Altera o arquivo Cors para especifica o dominio da vercel

--- backend/src/main/java/com/brutosburguer/backend/configuration/CorsConfiguration.java
+++ backend/src/main/java/com/brutosburguer/backend/configuration/CorsConfiguration.java
@@ -8,9 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                 registry.addMapping("/**")
+                .allowedOrigins("https://brutos-burguer-v1-frontend.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 
 }
