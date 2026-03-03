a817f0c5b7b4d78eed508fdc90b0ce288935e3c8
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2026-03-03 20:46:28 -0300

    BRUTOS28 Criando arquivo data.sql para insercao no Banco para tabela Roles

--- backend/src/main/java/com/brutosburguer/backend/repository/FuncionariosRepository/GerenteRepository.java
+++ backend/src/main/java/com/brutosburguer/backend/repository/FuncionariosRepository/GerenteRepository.java
@@ -3,5 +3,6 @@ package com.brutosburguer.backend.repository.FuncionariosRepository;
 import com.brutosburguer.backend.entity.Funcionarios.Gerente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//TODO Verificar o ID que não existe na Entity Gerente
 public interface GerenteRepository extends JpaRepository<Gerente, Long> {
 }
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_roles VALUES(1, 'admin');
+INSERT INTO tb_roles VALUES(2, 'gerente');
