5ddbaff68c43cd0dbb57f1067fdc2fc03d0d0d4e
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2026-03-03 21:30:43 -0300

    BRUTOS29 Cria arquivo datasource para insercao simples na tabela roles para simular dados iniciais

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO tb_roles VALUES(1, 'admin');
-INSERT INTO tb_roles VALUES(2, 'gerente');
+INSERT INTO brutos_burguer_dev.tb_roles(role_id, nome) VALUES (1, 'ADMIN');
+INSERT INTO brutos_burguer_dev.tb_roles(role_id, nome) VALUES (2, 'GERENTE');
